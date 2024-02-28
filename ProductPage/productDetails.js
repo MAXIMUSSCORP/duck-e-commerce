74509f914a50a9959e768e030155cae63706a3b5
maximusscorp <zaid.zk467@gmail.com>
2024-02-28 19:25:41 -0400

    cleaned up code for easier readability and understanding

--- ProductPage/productDetails.js
+++ ProductPage/productDetails.js
@@ -1,117 +1,85 @@
 let id;
+
+let cart;
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if cart exists in local storage
+    if (localStorage.getItem('cart') === null) {
+        cart = {
+            items: {
+                "1": 0,
+                "2": 0,
+                "3": 0,
+                "4": 0,
+                "5": 0,
+                "6": 0,
+                "7": 0,
+                "8": 0
+            }
+        };
+        localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+});
 function getQueryParam() {
     id = new URL(window.location.href).search.substring(1);
 }
 
-function dynamicContentDetails(ob)
-{
-    let mainContainer = document.createElement('div')
-    mainContainer.id = 'containerD'
-    document.getElementById('containerProduct').appendChild(mainContainer);
+function dynamicContentDetails(ob) {
+    const containerProduct = document.getElementById('containerProduct');
 
-    let imageSectionDiv = document.createElement('div')
-    imageSectionDiv.id = 'imageSection'
+    const mainContainerHTML = `
+        <div id="containerD">
+            <div id="imageSection">
+                <img id="imgDetails" src="${ob.preview}">
+            </div>
+            <div id="productDetails">
+                <h1>${ob.productName}</h1>
+                <div id="details">
+                    <h3>USD ${ob.price}</h3>
+                    <h3>Description</h3>
+                    <p>${ob.description}</p>
+                </div>
+                <div id="productPreview">
+                    <h3>Product Preview</h3>
+                    ${ob.productImages.map((src, index) => `<img id="previewImg${index}" src="${src}">`).join('')}
+                </div>
+                <div id="button">
+                    <button id="addToCartButton">${ob.category !== "limited" ? 'Add to Cart' : 'Out of Stock'}</button>
+                </div>
+            </div>
+        </div>
+    `;
 
-    let imgTag = document.createElement('img')
-    imgTag.id = 'imgDetails'
-    imgTag.src = ob.preview
-
-    imageSectionDiv.appendChild(imgTag)
-
-    let productDetailsDiv = document.createElement('div')
-    productDetailsDiv.id = 'productDetails'
-
-
-    let h1 = document.createElement('h1')
-    let h1Text = document.createTextNode(ob.productName)
-    h1.appendChild(h1Text)
-
-
-    let detailsDiv = document.createElement('div')
-    detailsDiv.id = 'details'
-
-    let h3DetailsDiv = document.createElement('h3')
-    let h3DetailsText = document.createTextNode('USD ' + ob.price)
-    h3DetailsDiv.appendChild(h3DetailsText)
-
-    let h3 = document.createElement('h3')
-    let h3Text = document.createTextNode('Description')
-    h3.appendChild(h3Text)
-
-    let para = document.createElement('p')
-    let paraText = document.createTextNode(ob.description)
-    para.appendChild(paraText)
-
-    let productPreviewDiv = document.createElement('div')
-    productPreviewDiv.id = 'productPreview'
-
-    let h3ProductPreviewDiv = document.createElement('h3')
-    let h3ProductPreviewText = document.createTextNode('Product Preview')
-    h3ProductPreviewDiv.appendChild(h3ProductPreviewText)
-    productPreviewDiv.appendChild(h3ProductPreviewDiv)
-
-    let i;
-    for (let i = 0; i < ob.productImages.length; i++) {
-        let imgTagProductPreviewDiv = document.createElement('img');
-        imgTagProductPreviewDiv.id = 'previewImg' + i; // Make IDs unique
-        imgTagProductPreviewDiv.src = ob.productImages[i];
-        imgTagProductPreviewDiv.onclick = (function(imageSrc) {
-            return function() {
-                document.getElementById("imgDetails").src = imageSrc;
-            };
-        })(ob.productImages[i]);
-        productPreviewDiv.appendChild(imgTagProductPreviewDiv);
-    }
-
-
-    let buttonDiv = document.createElement('div')
-    buttonDiv.id = 'button'
-
-    let buttonTag = document.createElement('button')
-    buttonDiv.appendChild(buttonTag)
+    containerProduct.insertAdjacentHTML('beforeend', mainContainerHTML);
 
     if (ob.category !== "limited") {
-        buttonText = document.createTextNode('Add to Cart')
-        buttonTag.onclick = function() {
-            let order = localStorage.getItem('orderId');
-            let counter = parseInt(localStorage.getItem('counter'), 10) || 0;
+        document.getElementById('addToCartButton').addEventListener('click', function() {
+            // Assuming 'id' is defined somewhere relevant to this context
+            cart.items[id] += 1;
+            console.log(cart);
+            localStorage.setItem('cart', JSON.stringify(cart));
 
-            if (order) {
-                order += " " + id;
-            } else {
-                order = id;
-            }
+            let counter = parseInt(localStorage.getItem('counter'), 10);
             counter += 1;
+            localStorage.setItem('counter', counter);
 
-            localStorage.setItem('orderId', order);
-            localStorage.setItem('counter', counter.toString());
-            console.log('Order ID:', order, 'Counter:', counter);
-            location.reload();
-        };
-
+            checkoutCounter();
+        });
     } else {
-        buttonText = document.createTextNode('Out of Stock')
-        buttonTag.disabled = true
-        buttonTag.style.backgroundColor = "grey"
+        const buttonTag = document.getElementById('addToCartButton');
+        buttonTag.disabled = true;
+        buttonTag.style.backgroundColor = "grey";
     }
-    buttonTag.appendChild(buttonText)
 
-
-    mainContainer.appendChild(imageSectionDiv)
-    mainContainer.appendChild(productDetailsDiv)
-    productDetailsDiv.appendChild(h1)
-    productDetailsDiv.appendChild(detailsDiv)
-    detailsDiv.appendChild(h3DetailsDiv)
-    detailsDiv.appendChild(h3)
-    detailsDiv.appendChild(para)
-    productDetailsDiv.appendChild(productPreviewDiv)
-
-
-    productDetailsDiv.appendChild(buttonDiv)
-
-
-    return mainContainer
+    // Adding event listeners to dynamically created product preview images for changing the main image
+    ob.productImages.forEach((imageSrc, index) => {
+        document.getElementById(`previewImg${index}`).addEventListener('click', () => {
+            document.getElementById("imgDetails").src = imageSrc;
+        });
+    });
 }
+
 
 let containerProduct = document.getElementById('containerProduct')
 
